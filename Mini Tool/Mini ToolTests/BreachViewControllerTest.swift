060da1f7ce10723626bad7fc4f34baf01696658d
NateCost <ilya.sakalou@gmail.com>
2020-05-13 19:12:11 +0300

    add factory sut method refactored tests

--- Mini Tool/Mini ToolTests/BreachViewControllerTest.swift
+++ Mini Tool/Mini ToolTests/BreachViewControllerTest.swift
@@ -8,86 +8,61 @@
 
 import Foundation
 import XCTest
+import WY_Mini_Tool_Engine
 @testable import Mini_Tool
 
 class BreachViewContllerTest: XCTestCase {
-  func test_viewDidLoad_noSegmentsToBreach_updatesStatus() {
-    let sut = BreachViewController(segmentsToBreach: [], segmentsToChoose: [])
-    
-    _ = sut.view
-    
-    XCTAssertEqual(sut.status, BreachStatus.noSegmentsToBreach)
-  }
-  
   func test_viewDidLoad_noSegmentsToChoose_updatesStatus() {
-    let segment = PixelMatrixSegment(value: "S1")
-    let sut = BreachViewController(
-      segmentsToBreach: [segment],
-      segmentsToChoose: []
-    )
-    
-    _ = sut.view
-    
+    let sut = makeSUT(segmentsToBreach: [PixelMatrixSegment(value: "S1")])
     XCTAssertEqual(sut.status, BreachStatus.noSegmentsToSelect)
   }
   
   func test_viewDidLoad_withSegmentsToBreachAndToChoose_updatesStatus() {
-    let segment = PixelMatrixSegment(value: "S1")
-    let sut = BreachViewController(
-      segmentsToBreach: [segment],
-      segmentsToChoose: [segment]
+    let sut = makeSUT(
+      segmentsToBreach: [PixelMatrixSegment(value: "S1")],
+      segmentsToChoose: [PixelMatrixSegment(value: "S1")]
     )
-    
-    _ = sut.view
-    
     XCTAssertEqual(sut.status, BreachStatus.breaching)
   }
   
-  func test_viewDidLoad_withNoSegmentsToBreach_segmentsToBreachStackIsEmpty() {
-    let sut = BreachViewController(
-      segmentsToBreach: [],
-      segmentsToChoose: []
-    )
-    
-    _ = sut.view
-    
+  func test_viewDidLoad_withNoSegmentsToBreach() {
+    let sut = makeSUT(segmentsToBreach: [])
     XCTAssertEqual(sut.segmentsToBreachStackView.subviews.count, 0)
+    XCTAssertEqual(sut.status, BreachStatus.noSegmentsToBreach)
   }
   
   func test_viewDidLoad_with2SegmentsToBreach_segmentsToBreachStackHas2Subviews() {
-    let segment1 = PixelMatrixSegment(value: "S1")
-    let segment2 = PixelMatrixSegment(value: "S2")
-    let segmentsToBreach = [segment1, segment2]
+    let sut = makeSUT(
+      segmentsToBreach: [
+        PixelMatrixSegment(value: "S1"),
+        PixelMatrixSegment(value: "S2")
+      ]
+    )
+    XCTAssertEqual(sut.segmentsToBreachStackView.subviews.count, 2)
+  }
+  
+  func test_viewDidLoad_collectionViewLoadsSegments() {
+    let sut = makeSUT(
+      segmentsToBreach: [],
+      segmentsToChoose: [PixelMatrixSegment(value: "S")]
+    )
+    XCTAssertEqual(sut.segmentsToChooseCollectionView.numberOfItems(inSection: 0), 1)
+    XCTAssertEqual(
+      makeSUT(
+        segmentsToChoose: []
+      ).segmentsToChooseCollectionView.numberOfItems(inSection: 0), 0
+    )
+  }
+  
+  func makeSUT(
+    segmentsToBreach: [Segment] = [],
+    segmentsToChoose: [Segment] = []
+  ) -> BreachViewController {
     let sut = BreachViewController(
       segmentsToBreach: segmentsToBreach,
-      segmentsToChoose: []
+      segmentsToChoose: segmentsToChoose
     )
-    
     _ = sut.view
-    
-    XCTAssertEqual(sut.segmentsToBreachStackView.subviews.count, segmentsToBreach.count)
-  }
-  
-  func test_viewDidLoad_withNoSegmentsToChoose_collectionViewIsEmpty() {
-    let sut = BreachViewController(
-      segmentsToBreach: [],
-      segmentsToChoose: []
-    )
-    
-    _ = sut.view
-    
-    XCTAssertEqual(sut.segmentsToChooseCollectionView.numberOfItems(inSection: 0), 0)
-  }
-  
-  func test_viewDidLoad_withOneSegmentToChoose_collectionViewHasOneCell() {
-    let segment = PixelMatrixSegment(value: "S")
-    let sut = BreachViewController(
-      segmentsToBreach: [],
-      segmentsToChoose: [segment]
-    )
-    
-    _ = sut.view
-    
-    XCTAssertEqual(sut.segmentsToChooseCollectionView.numberOfItems(inSection: 0), 1)
+    return sut
   }
 }
